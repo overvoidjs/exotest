680cc818c72b69fb7e75e4290ec4bf8aa0627a90
Jack <loisoj@gmail.com>
2020-06-09 13:42:20 +0300

    post_it update

--- exo.php
+++ exo.php
@@ -18,15 +18,18 @@ class Exo {
   }
 
   //отправка POST с параметрами по адрессу
-  public function post_it($url, $params){
+  public function post_it($url, $data){
 
-    $result = file_get_contents($url, false, stream_context_create(array(
-          'http' => array(
-              'method'  => 'POST',
-              'header'  => 'Content-type: application/x-www-form-urlencoded',
-              'content' => http_build_query($params)
-          )
-      )));
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
 
       echo $result;
   }
