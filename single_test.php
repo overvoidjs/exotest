83195b42e8d102f8a2a4628021d3ccde240f4e6d
Overvoid <loisoj@gmail.com>
2020-08-20 18:08:50 +0300

    screen with fail

--- single_test.php
+++ single_test.php
@@ -4,6 +4,6 @@ require 'exo.php';
 
 $i = new Exo;
 
-$url = 'http://localhost:7888/catalog/instrumentyi/';
+$url = 'http://localhost:7888/catalog/instrumentyi222/';
 
-$i->is_ok($url,'<div class="product-card-name">');
+$i->is_ok($url,'<div class="product-card-name1">');
